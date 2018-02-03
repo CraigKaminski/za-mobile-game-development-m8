31c82a36416442a3f9ab582d69a940db09d3322a
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-02-02 23:11:30 -0500

    Implement Bullet prefab

--- src/prefabs/Bullet.ts
+++ src/prefabs/Bullet.ts
@@ -1,5 +1,21 @@
+import Game from '../states/Game';
+
 export default class Bullet extends Phaser.Sprite {
-  constructor(game: Phaser.Game, x: number, y: number) {
-    super(game, x, y, 'bullet');
+  public body: Phaser.Physics.Arcade.Body;
+  private state: Game;
+
+  constructor(state: Game, x: number, y: number) {
+    super(state.game, x, y, 'bullet');
+
+    this.state = state;
+
+    this.game.physics.arcade.enable(this);
+    this.body.velocity.x = 100;
+  }
+
+  public update() {
+    if (this.x >= this.game.width) {
+      this.kill();
+    }
   }
 }
