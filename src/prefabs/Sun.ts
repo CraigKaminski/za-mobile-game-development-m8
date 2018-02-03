86bff7f51af9f7a3ee6146ed2c14a99a9f80be48
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-02-03 09:03:09 -0500

    Implement Sun prefab

--- src/prefabs/Sun.ts
+++ src/prefabs/Sun.ts
@@ -1,5 +1,55 @@
+import Game from '../states/Game';
+
 export default class Sun extends Phaser.Sprite {
-  constructor(game: Phaser.Game, x: number, y: number) {
-    super(game, x, y, 'sun');
+  public body: Phaser.Physics.Arcade.Body;
+  private state: Game;
+  private sunExpirationTimer: Phaser.Timer;
+
+  constructor(state: Game, x: number, y: number) {
+    super(state.game, x, y, 'sun');
+
+    this.state = state;
+
+    this.game.physics.arcade.enable(this);
+
+    this.animations.add('shine', [0, 1], 10, true);
+    this.play('shine');
+
+    this.anchor.setTo(0.5);
+
+    this.inputEnabled = true;
+    this.input.pixelPerfectClick = true;
+    this.events.onInputDown.add(() => {
+      console.log('sun collected');
+
+      this.kill();
+    });
+
+    this.sunExpirationTimer = this.game.time.create(false);
+    this.reset(x, y);
+  }
+
+  public kill() {
+    super.kill();
+
+    this.sunExpirationTimer.stop();
+
+    return this;
+  }
+
+  public reset(x: number, y: number) {
+    super.reset(x, y);
+
+    this.scheduleExpiration();
+  }
+
+  private scheduleExpiration() {
+    const expirationTime = 2 + Math.random() * 4;
+
+    this.sunExpirationTimer.start();
+
+    this.sunExpirationTimer.add(Phaser.Timer.SECOND + expirationTime, () => {
+      this.kill();
+    });
   }
 }
